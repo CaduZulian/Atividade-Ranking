c307a9e1d36e13f3ebe616d26770a564aacefab7
Carlos Eduardo Teixeira Zulian <71992281+CaduZulian@users.noreply.github.com>
2024-05-27 20:53:58 -0300

    feat: added default style and finished login page

---
+++ Auth/Login/backend.php
@@ -0,0 +1,29 @@
+<?php
+$username = $_POST["ra"];
+$senha = $_POST["password"];
+
+include('../../Config/conexao_bd.php');
+$db = new Database();
+$conn = $db->connect();
+
+if ($conn) {
+  try {
+    $query = "SELECT * FROM `alunos` WHERE `aluno_ra` = " . $username . " AND `aluno_senha` = '" . $senha . "'";
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+
+    $retorno_do_banco = $stmt->fetch(); 
+    if ($retorno_do_banco) {
+      session_start();
+      $_SESSION["username"] = $retorno_do_banco['aluno_ra'];
+      $_SESSION["nome"] = $retorno_do_banco['aluno_nome'];
+      header("Location: ../../Home");
+    } else {
+      echo "Aluno não encontrado. Tente novamente";
+    }
+  } catch (PDOException $e) {
+    echo "Erro na consulta: " . $e->getMessage();
+  }
+} else {
+  echo "Falha na conexão.";
+}
